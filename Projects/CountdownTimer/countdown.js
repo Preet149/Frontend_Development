34263f8334273d9a09f746793095f50787f8f274
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-05-30 18:12:42 +0200

    Added one function for the clear button

---
+++ Projects/CountdownTimer/countdown.js
@@ -0,0 +1,7 @@
+//Sets the timer back to 00 00 00
+function settoinitialstate() {
+   document.getElementById("hours").textContent = "00"
+   document.getElementById("minutes").textContent = "00"
+   document.getElementById("seconds").textContent = "00"
+}
+
