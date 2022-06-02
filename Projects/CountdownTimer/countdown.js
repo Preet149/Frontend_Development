f732735e7bff452d14937faa5ca20e45f7c00ead
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-06-02 15:19:35 +0200

    Made one change in the settooinitialstate function

--- Projects/CountdownTimer/countdown.js
+++ Projects/CountdownTimer/countdown.js
@@ -6,9 +6,7 @@ let counterhr1 = 0
 let counterhr2 = 0
 //Sets the timer back to 00 00 00
 function settoinitialstate() {
-   document.getElementById("hours").textContent = "00"
-   document.getElementById("minutes").textContent = "00"
-   document.getElementById("seconds").textContent = "00"
+   window.location.reload()
 }
 
 function decrementSeconds() {
