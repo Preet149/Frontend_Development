b8ef744ee2452aa036b44d83fbb2561fc7478a24
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-06-06 17:29:55 +0200

    Add some code of line

--- Projects/Calculator/calculator.js
+++ Projects/Calculator/calculator.js
@@ -1,3 +1,44 @@
+let currentvalue = 0
+let calculation = ""
+let operations = []
+let buttons = [0,1,2,3,4,5,6,7,8,9]
+let history = []
+
+function saveDigit(digit) {
+   if (currentvalue === 0) {
+        currentvalue = buttons[digit]
+   } else {
+        currentvalue += "" + buttons[digit]
+   }
+   displayDigits()
+}
+
+function saveOperation(operation) {
+     switch(operation) {
+          case '%' : operations.push(operation)
+                     break
+          case 'x' : operations.push(operation)
+                     break
+          case '-' : operations.push(operation)
+                     break
+          case '+' : operations.push(operation)
+                     break
+          default:
+     }
+     saveCurrentValue()
+}
+
+function saveCurrentValue() {
+     history.push(currentvalue)
+     currentvalue = 0
+     //Mit Stack arbeiten
+}
+
+function displayDigits() {
+     document.getElementById("display-resultat").textContent = currentvalue
+}
+
 function setToZero() {
-     document.getElementById("display-digit").textContent = "0"
+     currentvalue = 0
+     window.location.reload()
 }
