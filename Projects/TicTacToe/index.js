d9521227ad69a22de307215eeaac81c5fb166699
Preet149 <82959745+Preet149@users.noreply.github.com>
2022-06-13 16:14:46 +0200

    Make a function to set the crosses

--- Projects/TicTacToe/index.js
+++ Projects/TicTacToe/index.js
@@ -5,3 +5,30 @@ function switchToNextPage() {
 function returnToHomePage() {
     window.location.href="homepage.html"
 }
+
+function setCross() {
+     let pos = document.getElementById("position").value
+
+     switch(pos) {
+         case "top-left": document.getElementById("top-left").textContent = "X"
+                          break;
+         case "top": document.getElementById("top").textContent = "X"
+                     break;
+         case "top-right": document.getElementById("top-right").textContent = "X"
+                           break;
+         case "middle-left": document.getElementById("middle-left").textContent = "X"
+                             break;
+         case "middle": document.getElementById("middle").textContent = "X"
+                        break;
+         case "middle-right": document.getElementById("middle-right").textContent = "X"
+                              break;
+         case "bottom-left": document.getElementById("bottom-left").textContent = "X"
+                             break;
+         case "bottom": document.getElementById("bottom").textContent = "X"
+                        break;
+         case "bottom-right": document.getElementById("bottom-right").textContent = "X"
+                              break;
+         default: break;
+     }
+
+}
